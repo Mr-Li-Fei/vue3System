11e44cb9160a95a248efd084eca6aca4034dad8e
feli <feli@sonicwall.com>
2024-04-10 16:10:35 +0800

    fix: add element-plus and particles.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 // import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import Particles from "@tsparticles/vue3";
+import { loadSlim } from "@tsparticles/slim";
 
-import App from './App.vue'
-import router from './router'
+
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(ElementPlus);
+app.use(Particles, {
+    init: async engine => {
+        await loadSlim(engine);
+    }
+});
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
